4abde30693af07c65423d949f6cc930878254388
tardi <tardi.rachiman@btpnsyariah.com>
2025-11-06 09:55:20 +0700

    remove_slash

--- routes/gallery.go
+++ routes/gallery.go
@@ -9,9 +9,9 @@ import (
 func RegisterGalleryRoutes(router *gin.RouterGroup, c *initpkg.Container) {
 	gallery := router.Group("/gallery")
 	{
-		gallery.GET("/", c.GalleryHandler.GetAll)
+		gallery.GET("", c.GalleryHandler.GetAll)
 		gallery.GET("/:id", c.GalleryHandler.GetByID)
-		gallery.POST("/", c.Middlewares.Handle(), c.AtLeastAdmin.Handle(), c.GalleryHandler.Create)
+		gallery.POST("", c.Middlewares.Handle(), c.AtLeastAdmin.Handle(), c.GalleryHandler.Create)
 		gallery.PUT("/:id", c.Middlewares.Handle(), c.AtLeastAdmin.Handle(), c.GalleryHandler.Update)
 		gallery.DELETE("/:id", c.Middlewares.Handle(), c.AtLeastAdmin.Handle(), c.GalleryHandler.Delete)
 	}
